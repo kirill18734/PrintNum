fa1b9d2f70d3eb6aca3432a15223d91f946239fb
Kirill <kirill@gmail.com>
2025-09-05 14:58:27 +0300

    Переписал 2 расширения, для отправки на сервер и для скрытия определенных элементов

--- extensions/SendNumbersToLocalServer/content.js
+++ extensions/SendNumbersToLocalServer/content.js
@@ -1,15 +1,12 @@
 const previousCodes = new Set();
+const find_selector = "._shelfTag_1tkm1_2";
 const local_url_send_to_server = "http://127.0.0.1:5000/print_number";
-const find_selector = "._list_1bq3x_12"; // контейнер со всеми логами
-const timeout_sec = 300; // 300 мс между проверками
-
-function isValidCode(code) {
-  const valid = /^\d{1,}-\d+$/.test(code); // Пример: 422-4352
-  if (!valid) {
-    console.error(`❌ Невалидный код: '${code}'`);
-  }
-  return valid;
-}
+const work_url = "https://turbo-pvz.ozon.ru/receiving-v2/main";
+const timeout_find_number = 300;
+const timeout_find_url = 500;
+let prevUrl = undefined;
+let timerId;
+let first_run = false;
 
 function sendToServer(text) {
   fetch(local_url_send_to_server, {
@@ -23,39 +20,43 @@ function sendToServer(text) {
     });
 }
 
-function scanAndCompare() {
-  try {
-    const items = document.querySelectorAll("._shelfTag_1tkm1_2");
-    if (items.length === 0) {
-      console.warn("⚠️ Нет элементов для сканирования.");
-    }
-    items.forEach((item) => {
-      const code = item.textContent.trim();
-      if (isValidCode(code) && !previousCodes.has(code)) {
-        previousCodes.add(code);
-        sendToServer(code);
-      }
-    });
-  } catch (err) {
-    console.error("❌ Ошибка в процессе сканирования:", err);
-  }
-}
-
 function start() {
-  try {
-    const targetNode = document.querySelector(find_selector);
-    if (!targetNode) {
-      console.error(
-        `❌ Элемент с селектором '${find_selector}' не найден. Повторная попытка через ${timeout_sec} мс.`
-      );
-      setTimeout(start, timeout_sec);
-    } else {
-      scanAndCompare();
-      setInterval(scanAndCompare, timeout_sec);
-    }
-  } catch (err) {
-    console.error("❌ Ошибка при запуске процесса:", err);
+  const number = document.querySelector(find_selector);
+  if (!number) {
+    console.error(
+      `❌ Элемент с селектором '${find_selector}' не найден. Повторная попытка через ${timeout_find_number} мс.`
+    );
+    return;
+  }
+
+  let code = number.textContent;
+  // отправка на сервер
+  if (!previousCodes.has(code)) {
+    previousCodes.add(code);
+    sendToServer(code);
   }
 }
 
-start();
+setInterval(() => {
+  const currUrl = window.location.href;
+  if (currUrl != prevUrl) {
+    if (currUrl == work_url) {
+      if (!first_run) {
+        console.log("Скрипт на поиск ячеек Активирован");
+        first_run = true;
+      }
+      // запускаем цикл поиска ячеек
+      timerId = setInterval(start, timeout_find_number);
+    } else {
+      if (first_run) {
+        console.log("Скрипт на поиск ячеек Деактивирован");
+        first_run = false;
+      }
+      // останавливаем цикл поиска
+      if (timerId) {
+        clearInterval(timerId);
+      }
+    }
+    prevUrl = currUrl;
+  }
+}, timeout_find_url);
