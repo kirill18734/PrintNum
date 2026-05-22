823282aef67b076aa0008c711bb9c1ecb92a3c31
kirill <kirill@gmail.com>
2026-05-22 21:42:36 +0300

    разработка расширения

---
+++ extension_2/popup/script.js
@@ -0,0 +1,13 @@
+function rightTriangleIcon() {
+  return `
+<svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    class=""
+    viewBox="0 0 24 24"
+  >
+  <path fill="currentColor" d="m10 8 5 4-5 4z"></path>
+</svg>
+    `;
+}
