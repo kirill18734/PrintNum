be74f2180a8f17a94b5caefb9e9686e9443f020e
Kirill <148962239+kirill18734@users.noreply.github.com>
2026-08-26 18:39:55 +0000

    update qr-codes

--- extension/src/entrypoints/qrIssueAllOrder.content.ts
+++ extension/src/entrypoints/qrIssueAllOrder.content.ts
@@ -4,13 +4,14 @@ import { qrCommandsIssueAllOrder, SELECTORS } from "@/utils/constants";
 import { listening } from "@/utils/listener";
 import { waitLoadElement } from "@/utils/find";
 
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
 export default defineContentScript({
   matches: ["https://turbo-pvz.ozon.ru/*"],
 
   main() {
     let isRunning = false;
 
-    // ИСПРАВЛЕНО: Исправлена логика проверки элемента (if (!btn))
     async function clickByElem(selector: string, textValue: string) {
       const btn: any = await waitLoadElement(selector, textValue);
       if (!btn) return null;
@@ -41,6 +42,7 @@ export default defineContentScript({
 
             const success = await clickByElem(selector, text);
             if (!success) return; // Прерываем цепочку действий, если один из шагов сорвался
+            await delay(1000);
           }
         } while (true);
       } catch (err) {
